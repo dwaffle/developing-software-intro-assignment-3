f3054a7d29618982108b9335767165a91513d076
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-09-24 21:08:30 -0700

    Added comments to loadCustomerHouses

--- src/loadCustomerHouses.ts
+++ src/loadCustomerHouses.ts
@@ -3,8 +3,12 @@ import { Houses } from "./house/houses"
 import { buildWall} from "./wallCalculator/index"
 
 export function getHouseMatch(customerName:string) {
+    //Set the wallSuppliesCalcualtor
     Houses.setWallSuppliesCalculator((inches) => buildWall(inches))
+    //Get the houses in the save file.
     const savedHouses = Houses.getAll();
+    //If there isn't a house with a name that matches the customer, return that there's no house named that.
+    //Otherwise, find the house with a name that matches the customer, then return it.
     if(!savedHouses.get(customerName))
     {
         return "There is no house by that name.";
